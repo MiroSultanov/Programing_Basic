2365c1bb584327fa08523d9532704bf6acec6f2c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:24:39 +0300

    Update basketball _equipment.py

--- first_step_in_coding/basketball _equipment.py
+++ first_step_in_coding/basketball _equipment.py
@@ -1,4 +1,5 @@
-# Jesse decides he wants to play basketball, but he needs equipment to train. Write a program that calculates what costs Jesse will have if he starts training, knowing how much the basketball training fee is for a period of 1 year. Required equipment:
+# Jesse decides he wants to play basketball, but he needs equipment to train. Write a program that calculates what costs Jesse will have if he starts training,
+# knowing how much the basketball training fee is for a period of 1 year. Required equipment:
 # ⦁ Basketball sneakers - their price is 40% less than the fee for one year
 # ⦁ Basketball team - its price is 20% cheaper than that of sneakers
 # ⦁ Basketball - its price is 1/4 of the price of the basketball team
