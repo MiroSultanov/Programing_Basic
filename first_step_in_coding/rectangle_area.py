09124e9d8deeaa42e3f3663b766a07046e469aa8
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:29:45 +0300

    Update rectangle_area.py

--- first_step_in_coding/rectangle_area.py
+++ first_step_in_coding/rectangle_area.py
@@ -1,3 +1,5 @@
+# Write a console program that enters two integers (the sides of rectangles a and b) and calculates the face of a rectangle with these sides.
+
 a = int(input())
 b = int(input())
 area = a*b
