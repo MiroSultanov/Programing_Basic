31c056b9445b315844362b6fe36c24ecb1145bd6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:41:47 +0300

    Update deposit_calculator.py

--- first_step_in_coding/deposit_calculator.py
+++ first_step_in_coding/deposit_calculator.py
@@ -1,3 +1,15 @@
+# Write a program that calculates how much you will receive at the end of the deposit period at a certain interest rate. Use the following formula:
+# amount = deposited amount + term of the deposit * (deposited amount * annual interest rate) / 12)
+
+# Entrance
+# 3 lines are read from the console:
+# Deposit amount - real number in the interval [100.00… 10000.00]
+# Deposit term (in months) - integer in the interval [1… 12]
+# Annual interest rate - real number in the interval [0.00… 100.00]
+
+# Exit
+# Print the amount on the console at the end of the term.
+
 deposit = float(input())
 month = int(input())
 annual_interest_percent = float(input())
