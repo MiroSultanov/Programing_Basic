9ee4cd4238b1f3a3d1e3a33595c183345f36b206
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:45:18 +0300

    Update repainting.py

--- first_step_in_coding/repainting.py
+++ first_step_in_coding/repainting.py
@@ -1,3 +1,21 @@
+# Rumen wants to repaint the living room and has hired craftsmen for this purpose. Write a program that calculates the cost of repairs, given the following prices:
+# ⦁ Protective nylon - BGN 1.50 per square meter
+# ⦁ Paint - BGN 14.50 per liter
+# Боя Paint thinner - BGN 5.00 per liter
+# Just in case, to the necessary materials, Rumen wants to add another 10% of the amount of paint and 2 sq.m. nylon, of course BGN 0.40 for bags. 
+# The amount paid to the masters for 1 hour of work is equal to 30% of the sum of all costs for materials.
+
+# Entrance
+# The input is readable from the console and contains exactly 4 lines:
+# Required amount of nylon (in sq.m.) - an integer in the range [1 ... 100]
+# Required amount of paint (in liters) - an integer in the range [1… 100]
+# Diluent quantity (in liters) - integer in the range [1… 30]
+# The hours for which the masters will do the work - an integer in the interval [1… 9]
+
+# Exit
+# Print one line on the console:
+# "{sum of all expenses}"
+
 squared_meter_of_nylon = int(input())
 litters_of_paint = int(input())
 litters_of_thinner = int(input())
