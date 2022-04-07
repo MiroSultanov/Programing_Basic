6b9cfd712c759dbd459d4aed5e5fd31fc0d41dad
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:44:09 +0300

    Update supplies_for_school.py

--- first_step_in_coding/supplies_for_school.py
+++ first_step_in_coding/supplies_for_school.py
@@ -1,3 +1,20 @@
+# The school year has already started and the head of 10B class - Ani has to buy a certain number of packets of chemicals, 
+# packets of markers and detergent for cleaning the board. She is a regular customer of a bookstore, so there is a discount for her, 
+# which is a percentage of the total. Write a program that calculates how much money Annie will have to raise to pay the bill, keeping in mind the following price list:
+# ⦁ Package of chemicals - BGN 5.80.
+# ⦁ Package of markers - BGN 7.20.
+# ⦁ Detergent - BGN 1.20 (per liter)
+
+# Entrance
+# 4 numbers are read from the console:
+# Number of packages of chemicals - an integer in the range [0 ... 100]
+# Number of packet markers - integer in the range [0 ... 100]
+# Liters of board cleaner - integer in the range [0… 50]
+# Percentage reduction - an integer in the range [0 ... 100]
+
+# Exit
+# Print on the console how much money Annie will need to pay her bill.
+
 number_of_pens = int(input())
 number_of_markers = int(input())
 litter_of_detegrant = int(input())
