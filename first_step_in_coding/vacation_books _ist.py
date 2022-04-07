8794d302d427f68dc3372d7cc1df594615983509
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:42:58 +0300

    Update vacation_books _ist.py

--- first_step_in_coding/vacation_books _ist.py
+++ first_step_in_coding/vacation_books _ist.py
@@ -1,3 +1,15 @@
+# There are a number of books on Joro's list of required literature for the summer holidays. Because Joro prefers to play with friends outside, 
+# your task is to help him calculate how many hours a day he has to spend reading the necessary literature.
+
+# Entrance
+# 3 lines are read from the console:
+# Number of pages in the current book - integer in the interval [1… 1000]
+# Pages read in 1 hour - integer in the interval [1… 1000]
+# The number of days for which you must read the book - an integer in the range [1… 1000]
+
+# Exit
+# To print on the console the number of hours that Joro has to read each day.
+
 number_of_pages = int(input())
 pages = int(input())
 number_of_days = int(input())
