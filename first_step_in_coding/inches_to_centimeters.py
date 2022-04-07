c6ea6cac8f75acdf1a6e963122de4e74361aaf2f
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:30:44 +0300

    Update inches_to_centimeters.py

--- first_step_in_coding/inches_to_centimeters.py
+++ first_step_in_coding/inches_to_centimeters.py
@@ -1,3 +1,6 @@
+# Write a program that reads a real number from the console and converts it from inches to centimeters. 
+# To do this, multiply the inches by 2.54 (1 inch = 2.54 centimeters).
+
 inch = float(input())
 result = inch * 2.54
 print(result)
