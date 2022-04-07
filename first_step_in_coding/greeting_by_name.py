e5381c86a5ad6181ffde7d6488ec2e51024cc852
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:31:47 +0300

    Update greeting_by_name.py

--- first_step_in_coding/greeting_by_name.py
+++ first_step_in_coding/greeting_by_name.py
@@ -1,3 +1,5 @@
+# Write a program that reads text (person's name) from the console and prints "Hello, <name>!", Where <name> is the name entered from the console.
+
 name = input()
 
 print('Hello, ' + name + '!')
