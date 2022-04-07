297ca9d1a725c0093091e3ae8f9655e245b3a15e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:28:32 +0300

    Update nums_1...10.py

--- first_step_in_coding/nums_1...10.py
+++ first_step_in_coding/nums_1...10.py
@@ -1,3 +1,7 @@
+# Write a Python console program that prints the numbers 1 through 10 on separate lines on the console.
+
+# Write 10 print commands () one after the other to print the numbers 1 to 10.
+
 print(1)
 print(2)
 print(3)
