172b0565896b7ec7da3ed70b71127f41f99123ce
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-07 07:39:26 +0300

    Update usd_to_bgn.py

--- first_step_in_coding/usd_to_bgn.py
+++ first_step_in_coding/usd_to_bgn.py
@@ -1,3 +1,5 @@
+# Write a program for converting US dollars (USD) into Bulgarian levs (BGN). Use a fixed exchange rate between the dollar and the lev: 1 USD = 1.79549 BGN.
+
 usd = float(input())
 bgn = usd * 1.79549
 
