149fd2a809211f17c40b8b4e4de0e0e612ac21be
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:26:17 +0300

    Update excellent_result.py

--- conditional_statement/excellent_result.py
+++ conditional_statement/excellent_result.py
@@ -1,3 +1,6 @@
+# The first task in this topic is to write a console program that reads a rating (real number) entered by the user and prints "Excellent!" 
+# If the rating is 5.50 or higher.
+
 grade = float(input())
 if grade >= 5.50:
     print('Excellent!')
