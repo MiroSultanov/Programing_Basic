a48a7fcc2ca6cfef07e2ad36ace73cc4404a48b9
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:29:55 +0300

    Update speed_info.py

--- conditional_statement/speed_info.py
+++ conditional_statement/speed_info.py
@@ -1,3 +1,10 @@
+# Write a program that reads the speed (real number) entered by the user and prints speed information.
+# ⦁ At slow speeds up to 10 (inclusive) print "slow"
+# ⦁ At speeds above 10 and up to 50 (inclusive) print "average"
+# ⦁ At speeds above 50 and up to 150 (inclusive) print "fast"
+# ⦁ At speeds above 150 and up to 1000 (inclusive) print "ultra fast"
+# ⦁ Print "extremely fast" at higher speeds
+
 speed = float(input())
 if speed <= 10:
     print('slow')
