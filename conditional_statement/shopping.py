a46f5d8aad25d13cfd50414b627e125770fe70c5
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:39:38 +0300

    Update shopping.py

--- conditional_statement/shopping.py
+++ conditional_statement/shopping.py
@@ -1,3 +1,25 @@
+# Peter wants to buy N video cards, M CPU and P number of RAM. If the number of video cards is greater than that of the processors, 
+# it receives a 15% discount on the final bill. The following prices apply:
+# ⦁ Video card - BGN 250 / pc.
+# ⦁ Processor - 35% of the price of the purchased video cards / pc.
+# ⦁ Memory frame - 10% of the price of the purchased video cards / pc.
+# Calculate the amount needed to purchase the materials and calculate whether the budget will be enough.
+
+# Entrance
+# The entrance consists of four rows:
+# ⦁ Peter's budget - a real number in the interval [0.0… 100000.0]
+# ⦁ Number of video cards - integer in the range [0… 100]
+# ⦁ Number of processors - integer in the range [0… 100]
+# ⦁ The number of memory frames - integer in the range [0… 100]
+
+# Exit
+# 1 line is printed on the console, which should look like this:
+# ⦁ If the budget is sufficient:
+# "You have {residual budget} left left!"
+# ⦁ If the amount exceeds the budget:
+# "Not enough money! You need {necessary amount} left more!"
+# Format the result to the second decimal place.
+
 budget = float(input())
 number_cards = int(input())
 number_processor = int(input())
