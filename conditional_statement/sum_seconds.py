337bd4da9b134cb1a5349b9fecd9cb676704f828
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:33:16 +0300

    Update sum_seconds.py

--- conditional_statement/sum_seconds.py
+++ conditional_statement/sum_seconds.py
@@ -1,3 +1,6 @@
+# Three athletes finish in a matter of seconds (between 1 and 50). Write a program that reads the times of the competitors in seconds entered by the user and calculates
+# their total time in the format "minutes: seconds". Display seconds with leading zero (2 "02", 7 "07", 35 "35").
+
 first_time = int(input())
 second_time = int(input())
 third_time = int(input())
