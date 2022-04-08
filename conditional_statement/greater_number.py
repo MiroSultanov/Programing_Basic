66964cc98c53cb7f90f2a0a51624d758b68b44b4
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:27:01 +0300

    Update greater_number.py

--- conditional_statement/greater_number.py
+++ conditional_statement/greater_number.py
@@ -1,3 +1,5 @@
+# Write a program that reads two integers entered by the user and prints the larger of the two.
+
 first_name = int(input())
 second_name = int(input())
 if first_name > second_name:
