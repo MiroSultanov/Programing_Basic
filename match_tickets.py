38685fe86ea65e15463cc0e2b674118719278b08
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:20:43 +0300

    Delete match_tickets.py

--- match_tickets.py
+++
@@ -1,6 +0,0 @@
-budget = float(input())
-categories = input()
-number_of_people = int(input())
-if 1 < number_of_people <= 4
-    result = budget * 0.75
-    print(f'Yes! You have {result} leva left')
