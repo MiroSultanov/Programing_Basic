7dacd2edc2fb3f097bbe9d793e4dcd127f0574fc
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:28:28 +0300

    Update password_guess.py

--- conditional_statement/password_guess.py
+++ conditional_statement/password_guess.py
@@ -1,3 +1,7 @@
+# Write a program that reads a password (text) entered by the user and checks if the entered password matches the phrase "s3cr3t! P @ ssw0rd". 
+# In case of coincidence, display "Welcome".
+# In case of discrepancy, display "Wrong password!".
+
 user_password = 's3cr3t!P@ssw0rd'
 input_password = input()
 if user_password == input_password:
