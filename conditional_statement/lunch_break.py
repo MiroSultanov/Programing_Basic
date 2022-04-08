25a8746534f39e50c037f2e47a1c054042c46011
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-08 06:40:30 +0300

    Update lunch_break.py

--- conditional_statement/lunch_break.py
+++ conditional_statement/lunch_break.py
@@ -1,3 +1,21 @@
+# During the lunch break you want to watch an episode of your favorite series. 
+# Your task is to write a program that will find out if you have enough time to watch the episode. 
+# During the holiday you spend time for lunch and time for rest. Lunch time will be 1/8 of the rest time, and rest time will be 1/4 of the rest time.
+
+# Entrance
+# 3 lines are read from the console:
+# ⦁ Name of the series - text
+# ⦁ Episode duration - integer in the range [10… 90]
+# ⦁ Duration of the break - an integer in the range [10… 120]
+
+# Exit
+# Write one line on the console:
+# ⦁ If you have enough time to watch the episode:
+# "You have enough time to watch {series name} and left with {free time} minutes free time."
+# ⦁ If you do not have enough time:
+# "You don't have enough time to watch {series name}, you need {time} more minutes."
+# Time to round to the nearest whole number up.
+
 import math
 name_of_series = input()
 duration_of_episode = int(input())
