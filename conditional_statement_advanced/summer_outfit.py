5ea275b06f0f9054546d99013cedcaec93868467
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 10:18:39 +0300

    Update summer_outfit.py

--- conditional_statement_advanced/summer_outfit.py
+++ conditional_statement_advanced/summer_outfit.py
@@ -1,3 +1,21 @@
+# Summer is a season with very changeable weather and Victor needs your help. 
+# Write a program that, depending on the time of day and the degrees, will recommend Victor what clothes to wear. 
+# Your friend has different plans for each stage of the day, which require a different look - you can see them from the table.
+# Exactly two lines are read from the console:
+# ⦁ Degrees - integer;
+# ⦁ Time of day - text with three options "Morning", "Afternoon" or "Evening".
+
+# Time of day / degrees	       Morning	               Afternoon	             Evening
+# 10 <= градуси <= 18	     Outfit = Sweatshirt          Outfit = Shirt            Outfit = Shirt
+#                          Shoes = Sneakers	         Shoes = Moccasins         Shoes = Moccasins
+    
+# 18 < градуси <= 24	     Outfit = Shirt              Outfit = T-Shirt          Outfit = Shirt
+#                          Shoes = Moccasins	         Shoes = Sandals           Shoes = Moccasins
+
+# градуси >= 25	        Outfit = T-Shirt            Outfit = Swim Suit         Outfit = Shirt 
+#                         Shoes = Sandals	            Shoes = Barefoot           Shoes = Moccasins
+	
+
 degree = int(input())
 time_of_the_day = input()
 shoes = " "
