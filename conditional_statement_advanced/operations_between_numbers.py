8a655dfc0e970357bb3dd10355f588fdfa3182d5
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 10:27:44 +0300

    Update operations_between_numbers.py

--- conditional_statement_advanced/operations_between_numbers.py
+++ conditional_statement_advanced/operations_between_numbers.py
@@ -1,3 +1,31 @@
+# Write a program that reads two integers (N1 and N2) and an operator to perform a mathematical operation. 
+# Possible operations are: Addition (+), Subtraction (-), Multiplication (*), Division (/) and Modular Division (%). 
+# When adding, subtracting and multiplying the console, the result must be printed and whether it is even or odd. 
+# In ordinary division - the result. In modular division - the remainder. It should be borne in mind that the divisor can be equal to 0 (zero) 
+# and not divisible by zero. In this case, a special message must be printed.
+
+# Entrance
+# 3 lines entered by the user are read from the console:
+# ⦁ N1 - integer;
+# ⦁ N2 - integer;
+# ⦁ Operator - one character between: "+", "-", "*", "/", "%".
+
+# Exit
+# Print one line on the console:
+# ⦁ If the operation is addition, subtraction or multiplication:
+# } "{N1} {operator} {N2} = {result} - {even / odd}"
+# ⦁ If the operation is a division:
+# ⦁ "{N1} / {N2} = {result}" - result formatted to the second decimal place
+# ⦁ If the operation is a modular division:
+# ⦁ "{N1}% {N2} = {remainder}"
+# ⦁ In case of division by 0 (zero):
+# ⦁ "Cannot divide {N1} by zero"
+
+# Input
+# 10
+# 12
+# +
+
 N1 = int(input())
 N2 = int(input())
 symbol = input()
