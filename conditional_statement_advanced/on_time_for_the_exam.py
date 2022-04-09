faa868351ec420a36e6cbb4151fd9a37e139fa55
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 10:32:52 +0300

    Update on_time_for_the_exam.py

--- conditional_statement_advanced/on_time_for_the_exam.py
+++ conditional_statement_advanced/on_time_for_the_exam.py
@@ -1,3 +1,33 @@
+# A student must go to the exam at a certain time (for example, at 9:30). He comes to the examination hall at a certain time of arrival (for example 9:40).
+# It is considered that the student arrived on time if he arrived at the time of the exam or up to half an hour before. 
+# If he arrived more than 30 minutes earlier, he was early. If he came after the exam time, he was late. 
+# Write a program that reads exam time and arrival time and records whether the student arrived on time, was early or late, and by how many hours or 
+# minutes was early or late.
+
+# Entrance
+# The console reads 4 integers (one per line) entered by the user:
+# ⦁ The first line contains the exam time - an integer from 0 to 23;
+# ⦁ The second line contains a minute of the exam - an integer from 0 to 59;
+# ⦁ The third line contains the time of arrival - an integer from 0 to 23;
+# ⦁ The fourth line contains the minute of arrival - an integer from 0 to 59.
+
+# Exit
+# On the first line print:
+# ⦁ "Late" if the student arrives later than the exam time;
+# ⦁ "On time", if the student arrives exactly at the time of the exam or up to 30 minutes earlier;
+# "" Early "if the student arrives more than 30 minutes before the exam time.
+# If the student arrives at least one minute apart from the exam time, print on the following line:
+# ⦁ "mm minutes before the start" for arriving earlier by less than an hour;
+# "" Hh: mm hours before the start "for 1 hour or more early. Always print the minutes in 2 digits, for example "1:05";
+# "" Mm minutes after the start "for a delay of less than an hour;
+# "" Hh: mm hours after the start "for a delay of 1 hour or more. Always print the minutes with 2 digits, for example "1:03".
+        
+# Input
+# 9
+# 30
+# 9
+# 50
+
 hour_of_examine = int(input())
 minute_of_examine = int(input())
 hour_of_arriving = int(input())
