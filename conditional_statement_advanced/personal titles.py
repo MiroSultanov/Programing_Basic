fa51e8c358e2f6045b6be7f355a094bef5bfc7aa
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 09:51:11 +0300

    Update personal titles.py

--- conditional_statement_advanced/personal titles.py
+++ conditional_statement_advanced/personal titles.py
@@ -1,3 +1,16 @@
+# Write a console program that reads the age (real number) and gender ('m' or 'f') entered by the user and prints an address from the following:
+# ⦁ "Mr." - male (gender 'm') aged 16 or over
+# ⦁ "Master" - a boy (gender 'm') under 16 years
+# ⦁ "Ms." - a woman (gender 'f') aged 16 or over
+# "" Miss "- a girl (gender 'f') under 16 years
+
+# Sample input and output
+# input Output
+# 12f  Miss
+# 17m  Mr.
+# 25f  Ms.
+# 13.5m  Master
+
 age = float(input())
 gender = input()
 if gender == 'm':
