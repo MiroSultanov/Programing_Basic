48006a433c8a069e2251cfa9b2c972001eaa56c5
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 10:00:21 +0300

    Update cinema_ticket.py

--- conditional_statement_advanced/cinema_ticket.py
+++ conditional_statement_advanced/cinema_ticket.py
@@ -1,3 +1,8 @@
+# To write a program that reads the day of the week (text) - entered by the user and prints on the console the price of a movie ticket according to the day of the week:
+   
+# Monday Tuesday Wednesday Thursday Friday Saturday Sunday
+# 12       12     14          14      12     16      16
+
 day_of_week = input()
 if day_of_week == 'Monday' or day_of_week == 'Tuesday' or day_of_week == 'Friday':
     print(12)
