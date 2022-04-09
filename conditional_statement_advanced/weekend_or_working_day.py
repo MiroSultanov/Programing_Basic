40339d8970a93acc0466883ba2b3b1598c3fed4f
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 09:48:01 +0300

    Update weekend_or_working_day.py

--- conditional_statement_advanced/weekend_or_working_day.py
+++ conditional_statement_advanced/weekend_or_working_day.py
@@ -1,3 +1,17 @@
+# Write a program that reads the day of the week (text), in English - entered by the user. 
+# If the day is working prints on the console - "Working day", if it is a holiday - "Weekend". 
+# If text other than the day of the week is entered, print "Error".
+
+# Sample input and output
+# Input Output
+# Monday Working day
+
+# Input Output
+# Sunday Weekend
+
+# Input Output
+# April Error
+
 day_of_week = input()
 if day_of_week == 'Monday':
     print("Working day")
