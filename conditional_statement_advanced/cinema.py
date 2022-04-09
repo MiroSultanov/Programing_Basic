acdb966d0cb7488dffcded521f63b9947fe61a4d
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 10:13:39 +0300

    Update cinema.py

--- conditional_statement_advanced/cinema.py
+++ conditional_statement_advanced/cinema.py
@@ -1,3 +1,24 @@
+# In a movie theater, the chairs are arranged in a rectangular shape in r rows and c columns. There are three types of screenings with tickets at different prices:
+# ⦁ Premiere - premiere screening, at a price of BGN 12.00;
+# ⦁ Normal - standard screening, at a price of BGN 7.50;
+# ⦁ Discount - screening for children, pupils and students at a reduced price of BGN 5.00.
+# Write a program that reads the type of projection (text), the number of rows and the number of columns in the hall (integers) entered by the user,
+# and calculates the total ticket revenue for a full hall. The result should be printed in 2 characters after the decimal point.
+
+# Input          Output
+# Premiere
+# 10
+# 12	            1440.00 leva	 	
+
+# Normal
+# 21
+# 13	             2047.50 leva	
+
+# Discount
+# 12
+# 30	             1800.00 leva
+
+
 screening_type = input()
 number_of_rows = int(input())
 number_of_column = int(input())
