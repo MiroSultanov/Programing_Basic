d819352f46dec8b5432146a017484be21febc221
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 09:56:59 +0300

    Update number_in_range.py

--- conditional_statement_advanced/number_in_range.py
+++ conditional_statement_advanced/number_in_range.py
@@ -1,3 +1,12 @@
+# Write a program that checks whether the number entered by the user is in the range [-100, 100] and is different from 0 and displays "Yes" 
+# if it meets the conditions, or "No" if it is outside them.
+
+# Sample input and output
+
+#       -25        Yes
+#        0         No 
+#        25        Yes
+
 number = int(input())
 if -100 <= number <= 100 and number != 0:
     print('Yes')
