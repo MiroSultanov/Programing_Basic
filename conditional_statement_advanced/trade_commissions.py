a003deb7e509a6048679886a0050b27df5169cea
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 10:10:27 +0300

    Update trade_commissions.py

--- conditional_statement_advanced/trade_commissions.py
+++ conditional_statement_advanced/trade_commissions.py
@@ -3,7 +3,7 @@
 # city	0 ≤ s ≤ 500  	500 < s ≤ 1 000 	1 000 < s ≤ 10 000  	s > 10 000
 # Sofia	  5%	              7%	              8%	                12%
 # Varna	4.5%	             7.5%	              10%	                13%
-# Plovdiv	5.5%	              8%	              12%	                14.5%
+# Plovdiv	5.5%	          8%	              12%	                14.5%
 
 # Write a console program that reads the city name (text) and sales volume (real number) entered by the user, and calculates and displays the amount of the trade 
 # commission according to the table above. Display the result formatted to 2 digits after the decimal point. 
