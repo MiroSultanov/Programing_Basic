954f4ad3ca21a2b10f0a326670592d1780b28086
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-09 09:49:12 +0300

    Update animal_type.py

--- conditional_statement_advanced/animal_type.py
+++ conditional_statement_advanced/animal_type.py
@@ -1,3 +1,14 @@
+# Write a program that prints the class of the animal according to its name entered by the user.
+# ⦁ dog -> mammal
+# ⦁ crocodile, tortoise, snake -> reptile
+# ⦁ others -> unknown
+
+# Sample input and output
+# Input   Output
+# dog    mammal
+# snake  reptile
+# cat   unknown
+
 type_of_animal = input()
 if type_of_animal == 'dog':
     print("mammal")
