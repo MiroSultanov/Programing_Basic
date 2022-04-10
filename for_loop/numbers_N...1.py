41c8c9828f4e86a13db81952d151e7981f6aebfa
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-10 17:41:25 +0300

    Update numbers_N...1.py

--- for_loop/numbers_N...1.py
+++ for_loop/numbers_N...1.py
@@ -1,3 +1,5 @@
+# Write a program that reads a positive integer n entered by the user and prints the numbers n to 1 in reverse order. The number n entered will always be greater than 1.
+
 number = int(input())
 for num in range(number, 0, - 1):
     print(num)
