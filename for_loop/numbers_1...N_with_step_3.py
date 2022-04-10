f043d49284e9e1bf75409f967fb87b323dffbad2
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-10 17:39:06 +0300

    Update numbers_1...N_with_step_3.py

--- for_loop/numbers_1...N_with_step_3.py
+++ for_loop/numbers_1...N_with_step_3.py
@@ -1,3 +1,5 @@
+# Write a program that reads the number n entered by the user and prints the numbers from 1 to n through 3.
+
 number = int(input())
 for num in range(1, number + 1, 3):
     print(num)
