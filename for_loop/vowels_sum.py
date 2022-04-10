08bd0b3ccec80793419ea461bd7c7d033cd6f316
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-10 17:43:29 +0300

    Update vowels_sum.py

--- for_loop/vowels_sum.py
+++ for_loop/vowels_sum.py
@@ -1,3 +1,7 @@
+# Write a program that reads text (string) entered by the user and calculates and prints the sum of the values of the vowels according to the table below:
+# letter a	e	i	o	u
+# value  1  2   3   4   5
+
 text = (input())
 total_sum = 0
 for av in (text):
