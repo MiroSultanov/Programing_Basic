2dd0025908acf37a74af1f2288bac7d8c36fb211
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-10 17:40:37 +0300

    Update even _powers_of_2.py

--- for_loop/even _powers_of_2.py
+++ for_loop/even _powers_of_2.py
@@ -1,3 +1,5 @@
+# Write a program that reads the number n entered by the user and prints the even degrees of 2 ≤ 2^n: 2^0, 2^2, 2^4, 2^6,…, 2^n.
+
 number = int(input())
 for num in range(number + 1):
     if num % 2 == 0:
