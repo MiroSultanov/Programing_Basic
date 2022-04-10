9bf8579f697061bfc94d86c0c326ea5d54e1bd8e
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-10 17:44:59 +0300

    Update sum_numbers.py

--- for_loop/sum_numbers.py
+++ for_loop/sum_numbers.py
@@ -1,3 +1,8 @@
+# Write a program that reads n-number integers entered by the user and sums them.
+# ⦁ Enter the number of numbers n from the first line of the input.
+# ⦁ Enter an integer from the next n lines.
+# The program must read the numbers, sum them and print their sum.
+
 number = int(input())
 total_sum = 0
 
