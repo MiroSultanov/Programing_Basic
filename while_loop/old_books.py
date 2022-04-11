88bbdb8eadcd8f3e2e698f96b3b86fe289cb59ae
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-11 06:25:58 +0300

    Update old_books.py

--- while_loop/old_books.py
+++ while_loop/old_books.py
@@ -1,3 +1,13 @@
+# Ani goes to her hometown after a very long period abroad. On her way home, she sees her grandmother's old library and remembers her favorite book. 
+# Help Annie by writing a program in which she enters the book (text) she is looking for. Until Annie finds her favorite book or checks all the books in the library, 
+# the program must read each time on a new line the name of each subsequent book (text) that she checks. The books in the library are gone as soon as you receive the text
+# "No More Books".
+# ⦁ If he does not find the book he is looking for, print it in two lines:
+# ⦁ "The book you search is not here!"
+# ⦁ "You checked {number} books."
+# ⦁ If he finds his book, one line is printed:
+# ⦁ "You checked {number} books and found it."
+
 book_name = input()
 counter = 0
 book_is_found = False
