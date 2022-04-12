1fe4ed07cf9bc59ee14a426cc3ee239a73a31b9a
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-12 06:26:01 +0300

    Update multiplication_table.py

--- nested_loop/multiplication_table.py
+++ nested_loop/multiplication_table.py
@@ -1,3 +1,6 @@
+# Print on the console the multiplication table for the numbers 1 to 10 in the format:
+# "{first multiplier} * {second multiplier} = {result}".
+
 for n1 in range(1, 11):
     for n2 in range(1,11):
         result = n1 * n2
