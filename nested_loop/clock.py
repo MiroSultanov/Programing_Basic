cdd59badc754cc18982be6e776d516ff850d2533
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-12 06:24:56 +0300

    Update clock.py

--- nested_loop/clock.py
+++ nested_loop/clock.py
@@ -1,3 +1,5 @@
+# Write a program that prints the hours of the day from 0: 0 to 23:59, each on a separate line. The hours must be displayed in the format "{hour}: {minutes}".
+
 for h in range(24):
     for m in range(60):
         print(f"{h}:{m}")
