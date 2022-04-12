5bcda5d56c1df5e3a3655dfdda84668a6c3daf3c
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-04-12 06:26:35 +0300

    Update combinations.py

--- nested_loop/combinations.py
+++ nested_loop/combinations.py
@@ -1,3 +1,7 @@
+# Write a program that calculates how many solutions in natural numbers (including zero) the equation has:
+# x1 + x2 + x3 = n
+# The number n is an integer and is entered by the console.
+
 number = int(input())
 combination = 0
 for n1 in range(number + 1):
